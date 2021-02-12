1abe0bb5c856b1863ed7f90f8fa912fb32b1a527
GrapplingYou <77642984+GrapplingYou@users.noreply.github.com>
2021-02-13 00:32:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var btn1 = document.getElementById("smd")
+
+var text1 = document.getElementById("body")
+
+btn1.addEventListener("click",fun1)
+function fun1 (){   
+    text1.innerHTML = ('Geya')
+    text1.style.color = "red";
+}
+
+
+
+
+
+
+
+
